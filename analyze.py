53b85bf75803c53da84aeed7ab98e0bc31d30e3a
Rafał Szymanek <241436@student.pwr.edu.pl>
2020-05-29 11:51:22 +0200

    Complete pie chart of categories

--- analyze.py
+++ analyze.py
@@ -12,18 +12,47 @@ with open('categories.json') as json_file:
             'number': p['number']
         })
 
-print(varData['categories'][0]['name'])
+totalNumber = 0
+for category in varData['categories']:
+    totalNumber += int(category['number'])
+
+print("Liczba wszystkich produktów: " + str(totalNumber))
+
+lessThen = totalNumber*0.03
+print(lessThen)
+
+labels = []
+numbers = []
+otherLabels = []
+numOfOthers = 0
+
+for category in varData['categories']:
+    if(int(category['number']) <= lessThen):
+        otherLabels.append(category['name'])
+        numOfOthers += int(category['number'])
+
+    else:
+        labels.append(category['name'])
+        numbers.append(category['number'])
+
+varData['categories'].append({
+            'name': 'Inne',
+            'number': str(numOfOthers)
+        })
+labels.append('Inne')
+numbers.append(numOfOthers)
 
 
+print(varData)
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
 
-# # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-# sizes = [15, 30, 45, 10]
-# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+# explode = (0, 0.2, 0.1, 0, 0, 0, 0.3, 0.1, 0.2, 0, 0, 0.2, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-# fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-#         shadow=True, startangle=90)
-# ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-# plt.show()
+fig1, ax1 = plt.subplots()
+patches = ax1.pie(numbers, labels=labels, radius=1, autopct='%1.1f%%', startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+fig1.suptitle('Porównanie dostępnych kategorii', fontsize=14, fontweight='bold')
+ax1.set_title("Liczba wszystkich produktów: " + str(totalNumber))
+ttl = ax1.title
+ttl.set_position([.5, 1.05])
+plt.show()
