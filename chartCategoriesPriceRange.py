8888af8cdce83a9ee779c07f97533a8087d6934f
Rafał Szymanek <241436@student.pwr.edu.pl>
2020-05-31 12:11:01 +0200

    Calculate medians form all categories

---
+++ chartCategoriesPriceRange.py
@@ -0,0 +1,51 @@
+import json
+import statistics 
+
+data = {}
+data["Celowniki, lunety"] = [] 
+data["Części ASG"] = [] 
+data["Karabinki szturmowe"] = [] 
+data["Karabinki maszynowe"] = [] 
+data["Karabiny snajperskie"] = [] 
+data["Kulki ASG"] = [] 
+data["Magazynki"] = []
+data["Oporządzenie taktyczne"] = [] 
+data["Osprzęt ASG"] = [] 
+data["Pistolety i rewolwery"] = [] 
+data["Pistolety maszynowe"] = [] 
+data["Strzelby"] = [] 
+data["Pozostałe"] = [] 
+
+with open('allProductsInCategories.json') as json_file:
+    jsonData = json.load(json_file)
+
+    for oneCategory in data:
+        for p in jsonData[oneCategory]:
+            data[oneCategory].append({
+                'nazwa': p['nazwa'],
+                'cena': p['cena'],
+                'cena_z_dostawa': p['cena_z_dostawa'],
+                'licytacja': p['licytacja'],
+                'ile_osob_kupilo': p['ile_osob_kupilo'],
+                'allegro_smart': p['allegro_smart'],
+                'ssprzedawca': p['ssprzedawca'],
+                'cena': p['cena'],
+                'stan': p['stan']
+            })
+
+medianArray = []
+
+for oneCategory in data:
+    oneCategoryPrices = []
+    for i in data[oneCategory]:
+        arrPrice = []
+        for y in i['cena'] :
+            if y.isdigit():
+                arrPrice.append(y)
+        oneCategoryPrices.append(float(''.join(arrPrice))/100)
+    medianArray.append(statistics.median(oneCategoryPrices))
+
+
+print(medianArray)
+
+
